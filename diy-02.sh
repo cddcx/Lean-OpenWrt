4eb9d2e5923042928e01770b7c79e71c64e542bc
cddcx <84568016+cddcx@users.noreply.github.com>
2022-03-11 14:09:09 +0800

    Update diy-02.sh

--- diy-02.sh
+++ diy-02.sh
@@ -13,12 +13,12 @@ sed -i 's/bootstrap/opentopd/' feeds/luci/collections/luci/Makefile
 #rm -rf feeds/luci/themes/luci-theme-argon
 
 # 修改网络
-#sed -i 's/eth0/eth0 eth2 eth3/' package/base-files/files/etc/board.d/99-default_network
-#sed -i '2i # network config' package/default-settings/files/zzz-default-settings
-#sed -i "3i uci set network.wan.proto='pppoe'" package/default-settings/files/zzz-default-settings
-#sed -i "4i uci set network.wan.username='CD0283366379757'" package/default-settings/files/zzz-default-settings
-#sed -i "5i uci set network.wan.password='19701115'" package/default-settings/files/zzz-default-settings
-#sed -i "6i uci set network.wan.ifname='eth1'" package/default-settings/files/zzz-default-settings
-#sed -i "7i uci set network.wan6.ifname='eth1'" package/default-settings/files/zzz-default-settings
+sed -i 's/eth0/eth0 eth2 eth3/' package/base-files/files/etc/board.d/99-default_network
+sed -i '2i # network config' package/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.wan.proto='pppoe'" package/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.wan.username='CD0283366379757'" package/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.wan.password='19701115'" package/default-settings/files/zzz-default-settings
+sed -i "6i uci set network.wan.ifname='eth1'" package/default-settings/files/zzz-default-settings
+sed -i "7i uci set network.wan6.ifname='eth1'" package/default-settings/files/zzz-default-settings
 #set -i "8i uci network.lan.delegate='0'" package/default-settings/files/zzz-default-settings    # 去掉LAN口使用内置的 IPv6 管理
-#sed -i '9i uci commit network' package/default-settings/files/zzz-default-settings
+sed -i '8i uci commit network' package/default-settings/files/zzz-default-settings
