0c1d2304087845c7c4f9c1c287e3a10c6c46ac4f
cddcx <84568016+cddcx@users.noreply.github.com>
2022-05-22 08:45:35 +0800

    Update diy-01.sh

--- diy-01.sh
+++ diy-01.sh
@@ -51,8 +51,9 @@ rm -rf package/luci-theme-opentopd/README* package/luci-theme-opentopd/doc/
 
 # luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci/luci-app-passwall openwrt-passwall/luci-app-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall openwrt-passwall
 
 # luci-app-vssr及依赖
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
