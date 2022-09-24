2c854611679ac30bf3603bcb08da5ad67eb68f30
cddcx <84568016+cddcx@users.noreply.github.com>
2022-09-24 15:14:30 +0800

    Update diy-01.sh

--- diy-01.sh
+++ diy-01.sh
@@ -15,7 +15,7 @@
 
 # 修改内核
 sed -i 's/PATCHVER:=5.15/PATCHVER:=5.4/g' target/linux/x86/Makefile
-#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
 
 # 添加源
 #echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
