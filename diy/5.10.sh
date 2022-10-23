dc37f267d98bf918d8f1dd0189e8c82c36d24479
cddcx <84568016+cddcx@users.noreply.github.com>
2022-10-23 08:37:16 +0800

    Update 5.10.sh

--- diy/5.10.sh
+++ diy/5.10.sh
@@ -61,3 +61,7 @@ git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
 # luci-app-vssr及依赖
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+
+# luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+rm -rf package/luci-app-openclash/.svn
