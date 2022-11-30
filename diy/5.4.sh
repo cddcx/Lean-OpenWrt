cfe1a9f5efe121347326dbc32ea543740a266701
cddcx <84568016+cddcx@users.noreply.github.com>
2022-11-30 11:38:22 +0800

    Update 5.4.sh

--- diy/5.4.sh
+++ diy/5.4.sh
@@ -68,4 +68,4 @@ rm -rf package/luci-app-openclash/.svn
 
 # luci-app-v2ray
 git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
-git clone https://github.com/kuoruan/luci-app-v2ray.git
+git clone https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
